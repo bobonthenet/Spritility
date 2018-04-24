274257cf148acc23266c1e6c109dcceb2d2c7833
Robert Bedard <rbedard@plex.com>
2018-04-24 09:12:49 -0400

    Now outputs json

--- doit.js
+++ doit.js
@@ -15,17 +15,40 @@ let spriteJson = {
   }
 };
 
+const frameObj = {
+  "frame": {},
+	"rotated": false,
+	"trimmed": false,
+	"spriteSourceSize": {"x":0,"y":0},
+	"sourceSize": {},
+	"pivot": {"x":0.5,"y":0.5}
+}
+
 Spritesmith.run({src: sprites.map(sprite => spriteFolder + sprite)}, function handleResult(err, result) {
   if(err){
     console.log(err);
   }
-  fs.writeFileSync(__dirname + '/spritesheet.png', result.image);
-  console.log(result.coordinates);
-  // console.log(result.properties);
+  fs.writeFileSync('/spritesheet.png', result.image);
+
+  for (let key in result.coordinates){
+    const image = key.replace(spriteFolder, ""); 
+    spriteJson.frames[image] = frameObj;
+    spriteJson.frames[image].frame = {
+        "x": result.coordinates[key].x, 
+        "y": result.coordinates[key].y, 
+        "w": result.coordinates[key].width, 
+        "h": result.coordinates[key].height
+    }
+    spriteJson.frames[image].spriteSourceSize.w = result.coordinates[key].width;
+    spriteJson.frames[image].spriteSourceSize.h = result.coordinates[key].height;
+    spriteJson.frames[image].sourceSize.w = result.coordinates[key].width;
+    spriteJson.frames[image].sourceSize.h = result.coordinates[key].height;
+  };
 
   spriteJson.meta.size = {
     "w" : result.properties.width,
     "h" : result.properties.height
   };
-  console.log(JSON.stringify(spriteJson));
+  let json = JSON.stringify(spriteJson, null, 2);
+  fs.writeFileSync('spritesheet.json', json, 'utf8');
 });
